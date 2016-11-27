fbe1453d759af0558530b129056f0f73c58def1d
Pavel Lechev <pavel@schoolclubz.com>
2016-11-27 21:25:51 +0000

    Refactor to add "Apple" offer; offer test passes, original test fails

--- src/main/scala/Cart.scala
+++ src/main/scala/Cart.scala
@@ -5,7 +5,21 @@
 class Cart {
 
   def getTotalCost(items: Seq[String]): Double = {
-    items.foldRight(0.0)((i, t) => t + Products.products(i))
+    val grouped = items.groupBy(k => k)
+    // calc Apples:
+    var applesTotal = 0.0
+    if (grouped.isDefinedAt("Apple")) {
+      val allApples = grouped("Apple").size
+      applesTotal = allApples % 2 * Products.products("Apple") + allApples / 2 * Products.products("Apple")
+    }
+
+    var orangesTotal = 0.0
+    if (grouped.isDefinedAt("Orange")) {
+      val allOranges = grouped("Orange").size
+      orangesTotal = allOranges * Products.products("Orange")
+    }
+
+    applesTotal + orangesTotal
   }
 
 }
