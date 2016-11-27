e96abf1242202aab0485117f9891a8d797489394
Pavel Lechev <pavel@schoolclubz.com>
2016-11-27 22:16:02 +0000

    Improve total calculations - test for cart with apples not triggering an offer - pass

--- src/main/scala/Cart.scala
+++ src/main/scala/Cart.scala
@@ -10,14 +10,19 @@ class Cart {
     var applesTotal = 0.0
     if (grouped.isDefinedAt("Apple")) {
       val allApples = grouped("Apple").size
-      applesTotal = allApples % 2 * Products.products("Apple") + allApples / 2 * Products.products("Apple")
+      val applePrice = Products.products("Apple")
+      applesTotal = if (allApples >= 2) {
+        allApples % 2 * applePrice + (allApples / 2 * applePrice)
+      } else {
+        allApples * applePrice
+      }
     }
 
     var orangesTotal = 0.0
     if (grouped.isDefinedAt("Orange")) {
       val allOranges = grouped("Orange").size
       val orangePrice = Products.products("Orange")
-      orangesTotal = if (allOranges > 3) {
+      orangesTotal = if (allOranges >= 3) {
         allOranges % 3 * orangePrice + (allOranges / 3 * 2 * orangePrice)
       } else {
         allOranges * orangePrice
