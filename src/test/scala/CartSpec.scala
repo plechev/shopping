7970d88fe1cee06e9954c7a26303459d00fed66b
Pavel Lechev <pavel@schoolclubz.com>
2016-11-27 21:03:42 +0000

    Added failing test for "Apple" offer

--- src/test/scala/CartSpec.scala
+++ src/test/scala/CartSpec.scala
@@ -13,7 +13,16 @@ class CartSpec extends FlatSpec with Matchers {
     val total = cart.getTotalCost(items)
     val expected = Products.products("Apple") * 3 + Products.products("Orange")
     assert(expected == 2.05, "<- confirm expected cost")
-    assert(total == 2.05, "Confirm Cart totals")
+    assert(total == 2.05, "<- Confirm Cart totals")
+  }
+
+  it should """add offer "Buy one, get one free on Apples" """ in {
+    val items = Seq("Apple", "Apple", "Orange", "Apple")
+    val cart = new Cart()
+    val total = cart.getTotalCost(items)
+    val expected = Products.products("Apple") * 2 + Products.products("Orange") // only pay for 2 apples
+    assert(expected == 1.45, "<- confirm expected cost")
+    assert(total == 1.45, "< - Confirm Cart totals for Apple offer")
   }
 
 }
