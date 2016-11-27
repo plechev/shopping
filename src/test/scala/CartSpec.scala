19b6cc17b574c537c624415cd035a51b03c200c9
Pavel Lechev <pavel@schoolclubz.com>
2016-11-27 21:28:32 +0000

    Simplify original test to exclude offers; make it pass

--- src/test/scala/CartSpec.scala
+++ src/test/scala/CartSpec.scala
@@ -8,12 +8,12 @@ class CartSpec extends FlatSpec with Matchers {
 
 
   "Cart" should "take a list of items scanned at the till and outputs the total cost" in {
-    val items = Seq("Apple", "Apple", "Orange", "Apple")
+    val items = Seq("Apple", "Orange")
     val cart = new Cart()
     val total = cart.getTotalCost(items)
-    val expected = Products.products("Apple") * 3 + Products.products("Orange")
-    assert(expected == 2.05, "<- confirm expected cost")
-    assert(total == 2.05, "<- Confirm Cart totals")
+    val expected = Products.products("Apple") + Products.products("Orange")
+    assert(expected == 0.85, "<- confirm expected cost")
+    assert(total == expected, "<- Confirm Cart totals")
   }
 
   it should """add offer "Buy one, get one free on Apples" """ in {
@@ -22,7 +22,8 @@ class CartSpec extends FlatSpec with Matchers {
     val total = cart.getTotalCost(items)
     val expected = Products.products("Apple") * 2 + Products.products("Orange") // only pay for 2 apples
     assert(expected == 1.45, "<- confirm expected cost")
-    assert(total == 1.45, "< - Confirm Cart totals for Apple offer")
+    assert(total == expected, "< - Confirm Cart totals for Apple offer")
   }
+
 
 }
